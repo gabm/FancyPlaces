2c14a0f49beefed02876758fc519d66a7421886d
gabm <gabmtuc@gmail.com>
2015-05-31 14:17:13 +0200

    removed osmbonusback dependency as it is not needed anymore

--- app/src/main/java/com/gabm/fancyplaces/ui/OsmMarkerInfoWindow.java
+++ app/src/main/java/com/gabm/fancyplaces/ui/OsmMarkerInfoWindow.java
@@ -17,41 +17,100 @@
 
 package com.gabm.fancyplaces.ui;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.gabm.fancyplaces.R;
 
-import org.osmdroid.bonuspack.overlays.InfoWindow;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by gabm on 25/05/15.
  */
-public class OsmMarkerInfoWindow extends InfoWindow {
+public class OsmMarkerInfoWindow {
+
+    protected View mView;
+    protected boolean mIsVisible;
+    protected MapView mMapView;
+
 
 
     private TextView titleView = null;
 
-    public OsmMarkerInfoWindow(MapView mapView, OsmMarker parent) {
-        super(R.layout.marker_fancy_place, mapView);
+    public OsmMarkerInfoWindow(MapView mapView, OsmMarker parentMarker) {
+        this.mMapView = mapView;
+        this.mIsVisible = false;
 
-        titleView = (TextView) getView().findViewById(R.id.info_window_title);
+        ViewGroup parent = (ViewGroup) mapView.getParent();
+        LayoutInflater inflater = LayoutInflater.from(mapView.getContext());
 
-        titleView.setOnClickListener(parent);
+        this.mView = inflater.inflate(R.layout.marker_fancy_place, parent, false);
+        this.mView.setTag(this);
+
+
+        titleView = (TextView) mView.findViewById(R.id.info_window_title);
+        titleView.setOnClickListener(parentMarker);
 
     }
+
 
     public void setTitle(String title) {
         titleView.setText(title);
     }
 
-    @Override
-    public void onOpen(Object o) {
+
+    public void open(Object object, GeoPoint position, int offsetX, int offsetY) {
+        this.close();
+        MapView.LayoutParams lp = new MapView.LayoutParams(-2, -2, position, 8, offsetX, offsetY);
+        this.mMapView.addView(this.mView, lp);
+        this.mIsVisible = true;
+    }
+
+    public void close() {
+        if(this.mIsVisible) {
+            this.mIsVisible = false;
+            ((ViewGroup)this.mView.getParent()).removeView(this.mView);
+        }
 
     }
 
-    @Override
-    public void onClose() {
+    public boolean isOpen() {
+        return this.mIsVisible;
+    }
+
+    public static void closeAllInfoWindowsOn(MapView mapView) {
+        ArrayList opened = getOpenedInfoWindowsOn(mapView);
+        Iterator var3 = opened.iterator();
+
+        while(var3.hasNext()) {
+            OsmMarkerInfoWindow infoWindow = (OsmMarkerInfoWindow)var3.next();
+            infoWindow.close();
+        }
 
     }
+
+    public static ArrayList<OsmMarkerInfoWindow> getOpenedInfoWindowsOn(MapView mapView) {
+        int count = mapView.getChildCount();
+        ArrayList opened = new ArrayList(count);
+
+        for(int i = 0; i < count; ++i) {
+            View child = mapView.getChildAt(i);
+            Object tag = child.getTag();
+            if(tag != null && tag instanceof OsmMarkerInfoWindow) {
+                OsmMarkerInfoWindow infoWindow = (OsmMarkerInfoWindow)tag;
+                opened.add(infoWindow);
+            }
+        }
+
+        return opened;
+    }
+
+
 }
